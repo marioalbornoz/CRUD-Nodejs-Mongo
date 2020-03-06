18533d31eb447b477f4606aab164f020bc9c451f
marioalbornoz <mario.albornozc@utem.cl>
2020-03-06 01:27:20 -0300

    se agrega handlebars y motor de plantilla hbs

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 // initializations
@@ -8,6 +9,14 @@ const app = express();
 // Setting 
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layout '),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+}))
+
+app.set('view engine','.hbs');
 
 // Middlewares
 app.use(express.urlencoded({extended:false}));
@@ -16,7 +25,7 @@ app.use(express.urlencoded({extended:false}));
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index');
 })
 
 // Static Files
